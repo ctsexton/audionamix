10d2b3e1b8342c64d5840444c4fbaff195c5911c
Cam Sexton <cameron.t.sexton@gmail.com>
2018-07-29 00:23:10 -0700

    add gh pages dependencies - push-dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
+
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/audionamix/'
+  }
+} : {}
 module.exports = {
+  ...routerBase,
   /*
   ** Headers of the page
   */
