546ef74a8f6b2b5b06d86a00b8aab61e54ab97ae
Cam Sexton <cameron.t.sexton@gmail.com>
2018-08-07 10:31:02 -0700

    update logo path

--- pages/logo.vue
+++ pages/logo.vue
@@ -3,20 +3,30 @@
 	.top Vector graphics logo demo - same file retains sharpness at multiple sizes, reducing need to produce multiple graphic assets for different mediums.
 	.logo
 		h2 Small:
-		img(src="logo_display.svg" width="150px")
+		img(:src="contents.navigation.logo" width="150px")
 	.logo
 		h2 Medium:
-		img(src="logo_display.svg" width="300px")
+		img(:src="contents.navigation.logo" width="300px")
 	.logo
 		h2 Large:
-		img(src="logo_display.svg" width="450px")
+		img(:src="contents.navigation.logo" width="450px")
 	.logo
 		h2 Huge:
-		img(src="logo_display.svg" width="600px")
+		img(:src="contents.navigation.logo" width="600px")
 	.logo
 		h2 Massive:
-		img(src="logo_display.svg" width="900px")
+		img(:src="contents.navigation.logo" width="900px")
 </template>
+<script>
+import content from '~/static/content/index.md';
+export default {
+	data () {
+	    return {
+	        contents: content.attributes
+	    }
+	}
+}
+</script>
 <style scoped>
 h2 {
 	font-weight: 200;
